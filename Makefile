d9746472573146ae81d100d9efad62edbce9f8b4
Daniil Chizhevskii <enotprogrammist@mail.ru>
2020-06-06 12:14:12 +0300

    Add usage examples and update Makefile

--- Makefile
+++ Makefile
@@ -1,11 +1,10 @@
 BOOST_ROOT := /usr/include/boost
-PROGRAM := main
+SIMPLE := simple
+POOL := pool
 
-build: main.cpp
-	g++ main.cpp -o ${PROGRAM} -std=c++17 -I$(BOOST_ROOT) -lpthread -lcrypto -lboost_thread -lboost_chrono
-
-run:
-	./${PROGRAM}
+build:
+	g++ ${SIMPLE}.cpp -o ${SIMPLE} -std=c++17 -I$(BOOST_ROOT) -lpthread -lcrypto -lboost_thread -lboost_chrono
+	g++ ${POOL}.cpp -o ${POOL} -std=c++17 -I$(BOOST_ROOT) -lpthread -lcrypto -lboost_thread -lboost_chrono
 
 clean:
-	rm -r build
+	rm simple pool
---
+++ simple.cpp
@@ -0,0 +1,24 @@
+#include "AerospikeClient.h"
+#include <iostream>
+#include <variant>
+#include <iomanip>
+#include <vector>
+#include <map>
+
+int main() {
+  AerospikeClient client("127.0.0.1", 3000);
+  client.get("namespace", "key");
+  std::map<std::string, std::variant<std::string, long long, double>> result = client.result;
+  bool isEmpty = true;
+  for (auto const &iterator : result) {
+    isEmpty = false;
+    std::cout << iterator.first << ": \"";
+    std::visit([](const auto &elem) {
+      std::cout << elem << "\"." << std::endl;
+    }, iterator.second);
+  }
+  if (isEmpty) {
+    std::cout << "Empty." << std::endl;
+  }
+  return 0;
+}
